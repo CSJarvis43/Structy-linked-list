b3871def387e0491b2b52cef37af904a0d947c24
Charles Jarvis <csjarvis43@gmail.com>
2022-09-29 14:51:33 -0600

    added recursive solution

--- linkedListValues.js
+++ linkedListValues.js
@@ -11,3 +11,36 @@ const linkedListValues = (head) => {
 
     return result;
 }
+
+// Charlie's recursive solution
+
+const linkedListValuesRecursive = (head) => {
+    let result = [];
+
+    fillValues(head, result);
+
+    return result;
+
+}
+
+const fillValues = (head, result) => {
+    if (head === null) return;
+
+    result.push(head.val);
+    fillValues(head.next, result)
+}
+
+
+// Structy iterative solution
+
+const linkedListValuesStructy = (head) => {
+    const values = [];
+    let current = head;
+
+    while (current !== null) {
+        values.push(current.val);
+        current = current.next;
+    }
+
+    return values
+}
