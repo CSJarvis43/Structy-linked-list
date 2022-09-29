4573aca2a788569ff1c3f41a2ecc4d0865a2b0b1
Charles Jarvis <csjarvis43@gmail.com>
2022-09-29 16:12:14 -0600

    added recursive solution

---
+++ linkedListFind.js
@@ -0,0 +1,29 @@
+// Charlie's Iterative Solution
+
+const linkedListFind = (head, target) => {
+    let current = head;
+
+    while (current !== null) {
+        if (current.val === target) {
+            return true;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return false;
+}
+
+// Charlie's Recursive Solution
+
+const linkedListFindRecursive = (head, target) => {
+    
+    if (head === null) {
+        return false;
+    } else if (head.val === target) {
+        return true;
+    } else {
+        return linkedListFindRecursive(head.next, target);
+    }
+}
+
