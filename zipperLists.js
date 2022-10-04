1d68aa631afc1228f7aa9d961b1b3b761f53c7a8
Charlie Jarvis <csjarvis43@gmail.com>
2022-10-04 17:34:09 -0600

    added structy recursive solution

--- zipperLists.js
+++ zipperLists.js
@@ -28,3 +28,51 @@ const zipperLists = (head1, head2) => {
 
     return head1;
 };
+
+
+
+// Recursive solution initial attempt, excedding max call stack, need guidance
+// Does not function
+
+
+//     const zipperListsRecursive = (head1, head2, count = 0) => {
+
+//         let result = head1;
+        
+//         if (head1 === null && head2 === null) {
+//             if (count % 2 === 0) {
+//                 result.next = head2;
+//                 return zipperLists(head1.next, head2, count + 1)
+//             } else {
+//                 result.next = head1;
+//                 return zipperLists(head1, head2.next, count + 1)
+//         } 
+//             } else if (head1 !== null) {
+//             return result.next = head1;
+//         } else if (head2 !== null) {
+//             return result.next = head2;
+//         }
+
+//         return head1
+        
+        
+    
+// }
+
+//Structy Recursive Solution
+
+const zipperListsRecursive = (head1, head2) => {
+
+    if (head1 === null && head2 === null) return null
+    if (head1 === null) return head2;
+    if (head2 === null) return head1;
+
+    const n1 = head1.next;
+    const n2 = head2.next;
+
+    head1.next = head2;
+
+    head2.next = zipperListsRecursive(n1, n2);
+
+    return head1;
+}
