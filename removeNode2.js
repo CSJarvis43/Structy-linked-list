34d3ae14e94248d13d882551e66d7c75dfcae3c8
Charles Jarvis <csjarvis43@gmail.com>
2022-10-08 12:26:19 -0600

    iterative

---
+++ removeNode2.js
@@ -0,0 +1,24 @@
+// Remove node practice
+
+//Iterative 
+
+const removeNode = (head, targetVal) => {
+
+    if (head.val === targetVal) return head.next;
+
+    let current = head;
+    let prev = null;
+    
+    while (current !== null) {
+        if (current.val === targetVal) {
+            prev.next = current.next;
+            break;
+        }
+        prev = current;
+        current = current.next;
+    }
+    
+    return head;
+
+    return dummyHead.next;
+}
